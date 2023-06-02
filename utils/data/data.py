1f1361ef96a5c11384058d6f6743a27b3904bfb7
SungJun Lim <lsj9862@gmail.com>
2023-06-02 11:16:05 +0900

    fix data.py

--- utils/data/data.py
+++ utils/data/data.py
@@ -175,10 +175,6 @@ def get_cifar100(data_path='/mlainas/lsj9862/cifar100', batch_size=256, num_work
                         transforms.ToTensor(),
                         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
                     ])
-    tr_data = CIFAR10(data_path, train=True, transform=transform_train,
-                                            download=True)
-    te_data = CIFAR10(data_path, train=False, transform = transform_test,
-                                            download=True)
         
     ## Load Data
     tr_data = CIFAR100(data_path, train=True, transform=transform_train,
