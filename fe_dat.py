fix loader and hessian

fe_dat.py
@@ -7,14 +7,20 @@ import numpy as np
 
 # %%
 ## Settings
-MODEL_NAME = 'vitb16-i21k'
-DATASET = 'nabirds'
-DATA_PATH = f'/data1/lsj9862/data/{DATASET}'
+MODEL_NAME = 'resnet18-noBN'
+DATASET = 'cifar100'
+DATA_PATH = f'/data2/lsj9862/data/{DATASET}'
 BATCH_SIZE = 256
+RESUME = "/mlainas/lsj9862/exp_result/cifar100/resnet18-noBN/dnn-sgd/swag_lr/dnn-sgd_best_val.pt"
+SAVE_PATH = "/mlainas/lsj9862/exp_result/cifar100/resnet18-noBN/lswag-sam/swag_lr/fe_dat"
+DAT_PER_CLS = -1
+SEED = 0
+AUG = False
+
 
 # %%
-device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
-utils.set_seed(0)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+utils.set_seed(SEED)
 
 
 # %%
@@ -24,28 +30,41 @@ tr_loader, val_loader, te_loader, num_classes = utils.get_dataset(dataset=DATASE
                                                             batch_size=BATCH_SIZE,
                                                             num_workers=4,
                                                             use_validation=True,
-                                                            aug=True,
-                                                            fe_dat=None)
+                                                            aug=AUG,
+                                                            dat_per_cls = DAT_PER_CLS,
+                                                            seed = SEED)
 # %%
 ## Define model
-model = utils.get_backbone(MODEL_NAME, num_classes, device, pre_trained=True)
+model = utils.get_backbone(MODEL_NAME,
+                    num_classes = num_classes,
+                    device = device,
+                    pre_trained = True,
+                    last_layer = False)
 
-class Identity(nn.Module):
-    def __init__(self):
-        super(Identity, self).__init__()
+if RESUME is not None:
+    checkpoint = torch.load(RESUME)
+    model.load_state_dict(checkpoint['state_dict'])
         
-    def forward(self, x):
-        return x
+    class Identity(nn.Module):
+        def __init__(self):
+            super(Identity, self).__init__()
+            
+        def forward(self, x):
+            return x
+        
+    if MODEL_NAME == 'vitb16-i21k':
+        model.head = Identity()
+    elif MODEL_NAME in ['resnet18', 'resnet18-noBN', 'resnet50', 'resnet50-noBN', 'resnet101', 'resnet101-noBN']:
+        model.fc = Identity()
+    elif MODEL_NAME in ["wideresnet28x10", "wideresnet28x10-noBN"]:
+        raise ValueError("Add code for this")
 
-if MODEL_NAME == 'vitb16-i21k':
-    model.head = Identity()
-elif MODEL_NAME in ['resnet18', 'resnet18-noBN', 'resnet50', 'resnet50-noBN']:
-    model.fc = Identity()
 
 # %%
 ## Set save path
 import os
-os.makedirs(f"/mlainas/lsj9862/data/{DATASET}_{MODEL_NAME}_fe/", exist_ok=True)
+os.makedirs(SAVE_PATH, exist_ok=True)
+
 
 # %%
 ## Training data
@@ -63,8 +82,8 @@ with torch.no_grad():
 feature_list = torch.concat(feature_list, dim=0)
 target_list = torch.concat(target_list, dim=0)
 
-torch.save(feature_list, f"/mlainas/lsj9862/data/{DATASET}_{MODEL_NAME}_fe/tr_x.pt")
-torch.save(target_list, f"/mlainas/lsj9862/data/{DATASET}_{MODEL_NAME}_fe/tr_y.pt")
+torch.save(feature_list, f"{SAVE_PATH}/tr_fe_x.pt")
+torch.save(target_list, f"{SAVE_PATH}/tr_fe_y.pt")
 
 # %%
 ## Validation data
@@ -82,8 +101,8 @@ with torch.no_grad():
 feature_list = torch.concat(feature_list, dim=0)
 target_list = torch.concat(target_list, dim=0)
 
-torch.save(feature_list, f"/mlainas/lsj9862/data/{DATASET}_{MODEL_NAME}_fe/val_x.pt")
-torch.save(target_list, f"/mlainas/lsj9862/data/{DATASET}_{MODEL_NAME}_fe/val_y.pt")
+torch.save(feature_list, f"{SAVE_PATH}/val_fe_x.pt")
+torch.save(target_list, f"{SAVE_PATH}/val_fe_y.pt")
 
 # %%
 ## Test data
@@ -101,7 +120,7 @@ with torch.no_grad():
 feature_list = torch.concat(feature_list, dim=0)
 target_list = torch.concat(target_list, dim=0)
 
-torch.save(feature_list, f"/mlainas/lsj9862/data/{DATASET}_{MODEL_NAME}_fe/te_x.pt")
-torch.save(target_list, f"/mlainas/lsj9862/data/{DATASET}_{MODEL_NAME}_fe/te_y.pt")
+torch.save(feature_list, f"{SAVE_PATH}/te_fe_x.pt")
+torch.save(target_list, f"{SAVE_PATH}/te_fe_y.pt")
 
 # %%

run_sabtl.py
@@ -62,8 +62,8 @@ parser.add_argument("--use_validation", action='store_true',
 parser.add_argument("--dat_per_cls", type=int, default=-1,
             help="Number of data points per class in few-shot setting. -1 denotes deactivate few-shot setting (Default : -1)")
 
-parser.add_argument("--fe_dat", type=str, default=None, choices=[None, "resnet18-noBN", "vitb16-i21k"],
-            help = "Use Feature Extracted from Feature Extractor (Default : None)")
+# parser.add_argument("--fe_dat", type=str, default=None, choices=[None, "resnet18-noBN", "vitb16-i21k"],
+#             help = "Use Feature Extracted from Feature Extractor (Default : None)")
 #----------------------------------------------------------------
 
 ## Model ---------------------------------------------------------
@@ -146,9 +146,11 @@ args = parser.parse_args()
 # Set Device and Seed--------------------------------------------
 args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Device : {args.device}")
+args.last_layer = True
 
 if args.model.split("-")[-1] == "noBN":
     args.batch_norm = False
+    args.last_layer = False
 else:
     args.batch_norm = True
 
@@ -171,14 +173,14 @@ tr_loader, val_loader, te_loader, num_classes = utils.get_dataset(args.dataset,
                                                             batch_size = args.batch_size,
                                                             num_workers = args.num_workers,
                                                             use_validation = args.use_validation,
-                                                            fe_dat = args.fe_dat,
+                                                            # fe_dat = args.fe_dat,
                                                             dat_per_cls = args.dat_per_cls,
                                                             seed = args.seed)
-print(f"Load Data : {args.dataset} feature extracted from {args.fe_dat}")
+print(f"Load Data : {args.dataset}")
 #----------------------------------------------------------------
 
 # Define Model------------------------------------------------------
-model = utils.get_backbone(args.model, num_classes, args.device, args.pre_trained, args.fe_dat)   
+model = utils.get_backbone(args.model, num_classes, args.device, args.pre_trained, args.last_layer)   
 
 
 w_mean = torch.load(args.mean_path)
@@ -187,7 +189,7 @@ w_covmat = torch.load(args.covmat_path)
 sabtl_model = sabtl.SABTL(copy.deepcopy(model),
                         src_bnn=args.src_bnn,
                         pre_trained=args.pre_trained,
-                        fe_dat=args.fe_dat,
+                        # fe_dat=args.fe_dat,
                         w_mean = w_mean,
                         diag_only=args.diag_only,
                         w_var=w_var,

utils/sabtl/sabtl.py
@@ -16,7 +16,7 @@ class SABTL(torch.nn.Module):
         backbone,
         src_bnn = 'swag',
         pre_trained = True,
-        fe_dat = None, 
+        # fe_dat = None, 
         w_mean = None,
         diag_only = False,
         w_var=None,
@@ -48,20 +48,13 @@ class SABTL(torch.nn.Module):
                 raise RuntimeError("We need pre-trained weight to define model")
         
         # Set Last Layer Name
-        if fe_dat is not None:
-            for name, _ in self.backbone.named_modules():
-                self.last_layer_name = name
-        else:
-            self.last_layer_name = "Linear"
+        for name, _ in self.backbone.named_modules():
+            self.last_layer_name = name
 
         # Get total number of parameters and backbone shape which are updated during training
         self.num_params = 0
-        if fe_dat is not None:
-            for name, param in self.backbone.named_parameters():
-                if name.split('.')[0] == self.last_layer_name:
-                    self.num_params += param.numel()
-        else:
-            for param in self.backbone.parameters():
+        for name, param in self.backbone.named_parameters():
+            if name.split('.')[0] == self.last_layer_name:
                 self.num_params += param.numel()
 
         self.backbone_shape = self.full_model_shape[-2:]