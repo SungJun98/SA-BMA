let's start fixing kl term

utils/models/clip_img_enc.py
@@ -4,18 +4,18 @@ import torch.nn as nn
 
 import clip
 
+
 class resnet50_clip(nn.Module):
     def __init__(self, num_classes):
         super(resnet50_clip, self).__init__()
         clip_model, _ = clip.load("RN50")
         self.base = clip_model.visual
-        self.base.attnpool = None
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1,1))
-        self.fc = nn.Linear(in_features=2048, out_features=num_classes, bias=True)
+        self.fc = nn.Linear(in_features=1024, out_features=num_classes, bias=True).to(dtype=torch.float16)
+        
 
     def forward(self, x):
-        x = self.base(x)
-        x = self.avgpool(x)
+        x = self.base(x)        
+        x = torch.flatten(x, 1)
         return self.fc(x)
     
     
@@ -25,12 +25,11 @@ class resnet101_clip(nn.Module):
         super(resnet101_clip, self).__init__()
         clip_model, _ = clip.load("RN101")
         self.base = clip_model.visual
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1,1))
-        self.fc = nn.Linear(in_features=2048, out_features=num_classes, bias=True)
+        self.fc = nn.Linear(in_features=1024, out_features=num_classes, bias=True).to(dtype=torch.float16)
 
     def forward(self, x):
-        x = self.base(x)
-        x = self.avgpool(x)
+        x = self.base(x)        
+        x = torch.flatten(x, 1)
         return self.fc(x)
     
     
@@ -39,13 +38,12 @@ class vitb16_clip(nn.Module):
     def __init__(self, num_classes):
         super(vitb16_clip, self).__init__()
         clip_model, _ = clip.load("ViT-B/16")
-        self.base = clip_model.visual
-        self.fc_norm = nn.Identity()
-        self.head_drop = nn.Dropout(p=0.0, inplace=False)
-        self.head = nn.Linear(in_features=768, out_features=num_classes, ias=True)
+        self.base = clip_model.visual.float()
+        self.base.proj = None
+        self.head_drop = nn.Dropout(p=0.0, inplace=False) .float()
+        self.head = nn.Linear(in_features=768, out_features=num_classes, bias=True).float()
 
     def forward(self, x):
         x = self.base(x)
-        x = self.fc_norm(x)
         x = self.head_drop(x)
         return self.head(x)

utils/sabma/sabma.py
@@ -25,11 +25,9 @@ class SABMA(torch.nn.Module):
         var_clamp = 1e-16,
         tr_layer="nl_ll",
         pretrained_set = 'source',
-        alpha=1e-2
+        alpha=1e-4
     ):
-        """
-        TODO : last layer random initialization 코드 추가 (argument 받아서)
-        """
+
         super(SABMA, self).__init__()
         
         self.var_clamp = var_clamp