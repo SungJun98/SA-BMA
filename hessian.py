fix hessian.py, update evaluate.py, and add loss_surface_tmp.py

hessian.py
@@ -1,14 +1,16 @@
 import argparse
-import torch
-import pickle
-import os, copy
+import pickle, os, copy
+
 import utils.data.data as data
 import utils.utils as utils
+from utils.swag import swag, swag_utils
+
+import torch
 import numpy as np
 
 from hessian_eigenthings import compute_hessian_eigenthings
+import torch.nn.functional as F
 
-from utils.swag import swag, swag_utils
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -45,23 +47,20 @@ parser.add_argument("--dat_per_cls", type=int, default=-1,
 parser.add_argument(
     "--model",
     type=str, default='resnet18', required=True,
-    choices=['resnet18', 'resnet50', 'resnet101', 'wideresnet28x10', 'wideresnet40x10',
-            'resnet18-noBN', 'resnet50-noBN', 'resnet101-noBN', 'wideresnet28x10-noBN', 'wideresnet40x10-noBN',
-            "vitb16-i21k"],
+    choices=['resnet18', 'resnet18-noBN', "vitb16-i21k"],
     help="model name (default : resnet18)")
 
 parser.add_argument("--swag_load_path", type=str, default=None,
     help="path to load saved swag model (default: None)",)
 
+parser.add_argument("--vi_load_path", type=str, default=None,
+    help="path to load saved vi model (default: None)",)
+
+parser.add_argument("--sabma_load_path", type=str, default=None,
+    help="path to load saved sabma model (default: None)",)
+
 parser.add_argument("--load_path", type=str, default=None,
     help="path to load saved model (default: None)")
-
-parser.add_argument(
-    "--last_swag",
-    action='store_true',
-    default=False,
-    help ="When model trained with last swag (Default : False)"
-)
 
 parser.add_argument(
     "--pre_trained", action='store_true', default=True,
@@ -111,6 +110,8 @@ def load_swag_model_to_base_model(model, bma_sample):
     return model
 
 
+
+
 # Set Device and Seed------------------------------------------------------
 args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Device : {args.device}")
@@ -126,24 +127,20 @@ utils.set_seed(args.seed)
 #---------------------------------------------------------------------------
 
 # Load Data -----------------------------------------------------------------
-if not args.last_layer:
-    tr_loader, _, te_loader, num_classes = utils.get_dataset(dataset=args.dataset,
-                                                data_path=args.data_path,
-                                                dat_per_cls=args.dat_per_cls,
-                                                use_validation=args.use_validation, 
-                                                batch_size=args.batch_size,
-                                                num_workers=args.num_workers,
-                                                seed=args.seed,
-                                                aug=args.batch_norm,
-                                                )
-    if args.dat_per_cls >= 0:
-        print(f"Load Data : {args.dataset}-{args.dat_per_cls}shot")
-    else:
-        print(f"Load Data : {args.dataset}")
-        
+tr_loader, _, te_loader, num_classes = utils.get_dataset(dataset=args.dataset,
+                                            data_path=args.data_path,
+                                            dat_per_cls=args.dat_per_cls,
+                                            use_validation=args.use_validation, 
+                                            batch_size=args.batch_size,
+                                            num_workers=args.num_workers,
+                                            seed=args.seed,
+                                            aug=args.batch_norm,
+                                            )
+if args.dat_per_cls >= 0:
+    print(f"Load Data : {args.dataset}-{args.dat_per_cls}shot")
 else:
-    raise ValueError("Add code that loads the Feature Extracted dataset")
-    print(f"Load Data : Feature Extracted {args.dataset}")
+    print(f"Load Data : {args.dataset}")
+    
 #---------------------------------------------------------------------------
 
 ## Define Model--------------------------------------------------------------
@@ -166,6 +163,18 @@ if args.swag_load_path is not None:
     """
     # Get bma weights list
     bma_load_paths = sorted(os.listdir(args.swag_load_path))
+    
+elif args.sabma_load_path is not None:
+    bma_load_paths = sorted(os.listdir(args.sabma_load_path))
+    model_path = args.sabma_load_path.split("/")[:-2]
+    model_path = '/'+os.path.join(*model_path)
+    model_path = os.path.join(model_path, "sabma-sabma_best_val_model.pt" )
+    model = torch.load(model_path)
+    model = model.backbone
+
+elif args.vi_load_path is not None:
+    bma_load_paths = sorted(os.listdir(args.vi_load_path))
+
 else:
     checkpoint = torch.load(args.load_path)
     if hasattr(checkpoint, "temperature"):
@@ -180,22 +189,27 @@ model.to(args.device)
 ## Set save path ---------------------------------------------------------------
 if args.swag_load_path is not None:
     save_path = f"{args.swag_load_path}/performance"
-    os.makedirs(save_path, exist_ok=True)
+    
+elif args.sabma_load_path is not None:
+    save_path = f"{args.sabma_load_path}/performance"
+
+elif args.vi_load_path is not None:
+    save_path = f"{args.vi_load_path}/performance"
+
 else:
     save_path = args.load_path.split("/")[:-1]
     save_path = os.path.join(*save_path)
     save_path = f"/{save_path}/performance"
-    os.makedirs(save_path, exist_ok=True)
-
+os.makedirs(save_path, exist_ok=True)
 print(f"Save path : {save_path}")
 # ------------------------------------------------------------------------------
 
-model.eval()
+# model.eval()
 
 criterion = torch.nn.CrossEntropyLoss()
 
 ## Calculate Hessian ------------------------------------------------------------
-if args.swag_load_path is not None:
+if (args.swag_load_path is not None) or (args.vi_load_path is not None) or (args.sabma_load_path is not None):
     model_num_list = list(); acc_list = list(); ece_list = list(); nll_list = list()
     tr_cum_eigenval_list = list() ; tr_max_eigenval_list = list()
     for path in bma_load_paths:
@@ -203,10 +217,25 @@ if args.swag_load_path is not None:
         model_num = model_num.split("-")[-1]
         
         # get sampled model
-        bma_sample = torch.load(f"{args.swag_load_path}/{path}")
-        model = load_swag_model_to_base_model(model, bma_sample)
-
-        res = utils.eval(te_loader, bma_sample, criterion, args.device)
+        if args.swag_load_path is not None:
+            bma_sample = torch.load(f"{args.swag_load_path}/{path}")
+            model = load_swag_model_to_base_model(model, bma_sample)
+            # model = torch.load(f"{args.swag_load_path}/{path}")
+        elif args.vi_load_path is not None:
+            bma_sample = torch.load(f"{args.vi_load_path}/{path}")
+            model = bma_sample
+            # model = torch.load(f"{args.vi_load_path}/{path}")
+        elif args.sabma_load_path is not None:
+            bma_sample = torch.load(f"{args.sabma_load_path}/{path}")
+            model.load_state_dict(bma_sample, strict=False)
+            
+        if (args.swag_load_path) is not None or (args.vi_load_path is not None):
+            res = utils.eval(te_loader, bma_sample, criterion, args.device)
+        elif args.sabma_load_path is not None:
+            res = utils.eval(te_loader, model, criterion, args.device)
+        for p in model.parameters():
+            p.requires_grad_(True)
+        
         print(f"# {model_num} / Test Accuracy : {res['accuracy']:8.4f}% / ECE : {res['ece']} / NLL : {res['nll']}")
         
         model_num_list.append(model_num); acc_list.append(res['accuracy']); ece_list.append(res['ece']); nll_list.append(res['nll'])
@@ -217,7 +246,8 @@ if args.swag_load_path is not None:
                     tr_loader,
                     criterion,
                     num_eigenthings=args.num_eigen,
-                    mode="lanczos", #"power_iter"
+                    mode="lanczos",
+                    # mode="power_iter",
                     # power_iter_steps=50,
                     max_possible_gpu_samples=args.max_possible_gpu_samples,
                     # momentum=args.momentum,
@@ -226,12 +256,17 @@ if args.swag_load_path is not None:
 
             tr_cum_eigenval_list.append(tr_eigenvals)
             tr_max_eigenval_list.append(max(tr_eigenvals))
-            print(f"Successfully get {model_num}-th swag bma model eigenvalues for train set")
+            if args.swag_load_path is not None:
+                print(f"Successfully get {model_num}-th swag bma model eigenvalues for train set")
+            elif args.vi_load_path is not None:
+                print(f"Successfully get {model_num}-th vi bma model eigenvalues for train set")
+            elif args.sabma_load_path is not None:
+                print(f"Successfully get {model_num}-th sabma bma model eigenvalues for train set")
             print(f"Train Eigenvalues for {model_num}-th bma model : {tr_eigenvals}")
         except:
            print(f"Numerical Issue on {model_num}-th model with train data")
-           tr_cum_eigenval_list.append(9999999)
-           tr_max_eigenval_list.append(9999999)
+           tr_cum_eigenval_list.append(999999999)
+           tr_max_eigenval_list.append(999999999)
         
         # save results
         performance = dict({"model_num" : model_num_list,

run_sabma.py
@@ -11,6 +11,7 @@ import utils.utils as utils
 from utils import temperature_scaling as ts
 from utils.swag import swag, swag_utils
 from utils.sabma import sabma, sabma_utils
+import utils.data.data as data
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -184,6 +185,7 @@ if not args.ignore_wandb:
 #----------------------------------------------------------------
 
 # Load Data --------------------------------------------------------
+data_path_ood = args.data_path
 args.data_path = os.path.join(args.data_path, args.dataset)
 tr_loader, val_loader, te_loader, num_classes = utils.get_dataset(dataset=args.dataset,
                                                         data_path=args.data_path,
@@ -319,7 +321,7 @@ for epoch in range(start_epoch, int(args.epochs)+1):
         val_res = sabma_utils.bma_sabma(val_loader, sabma_model, args.val_mc_num,
                             num_classes, criterion, args.device,
                             bma_save_path=None, eps=1e-8, num_bins=50,
-                            validation=True, tr_layer=args.tr_layer
+                            validation=True, tr_layer=args.tr_layer, ood_loader=None
                             )
 
     time_ep = time.time() - time_ep
@@ -385,12 +387,25 @@ for epoch in range(start_epoch, int(args.epochs)+1):
 
 ## Test ------------------------------------------------------------------------------------------------------
 ##### Get test nll, Entropy, ece, Reliability Diagram on best model
+## Load Distributional shifted data
 # Load Best Model
 print("Load Best Validation Model (Lowest Loss)")
 state_dict_path = f"{args.save_path}/{args.method}-{args.optim}_best_val.pt"
 checkpoint = torch.load(state_dict_path)   
 sabma_model.load_state_dict(checkpoint["state_dict"])
 sabma_model.to(args.device)
+
+
+### Get temperature
+val_res = sabma_utils.bma_sabma(val_loader, sabma_model, 1,
+                    num_classes, criterion, args.device,
+                    bma_save_path=None, eps=args.eps, num_bins=args.num_bins,
+                    validation=False, tr_layer=args.tr_layer, ood_loader=None)
+scaled_model = ts.ModelWithTemperature(sabma_model, ens=True)
+scaled_model.set_temperature(val_loader, ens_logits=torch.tensor(val_res['logits']), ens_pred=torch.tensor(val_res['targets']))
+bma_temperature = scaled_model.temperature
+
+
 
 ### BMA prediction
 if args.no_save_bma:
@@ -402,7 +417,7 @@ else:
 bma_res = sabma_utils.bma_sabma(te_loader, sabma_model, args.bma_num_models,
                     num_classes, criterion, args.device,
                     bma_save_path=bma_save_path, eps=args.eps, num_bins=args.num_bins,
-                    validation=False, tr_layer=args.tr_layer)
+                    validation=False, tr_layer=args.tr_layer, ood_loader=None)
 bma_logits = bma_res["logits"]
 bma_predictions = bma_res["predictions"]
 bma_targets = bma_res["targets"]
@@ -413,18 +428,13 @@ bma_nll = bma_res["nll"]
 unc = utils.calibration_curve(bma_predictions, bma_targets, args.num_bins)
 bma_ece = bma_res['ece']
 
-# Temperature Scale
-scaled_model = ts.ModelWithTemperature(sabma_model, ens=True)
-scaled_model.set_temperature(val_loader, ens_logits=torch.tensor(bma_logits), ens_pred=torch.tensor(bma_targets))
-bma_temperature = scaled_model.temperature
+# Temperature Scaling
 bma_logits = torch.tensor(bma_logits) / bma_temperature.cpu()
 bma_predictions_ts = F.softmax(bma_logits, dim=1).detach().numpy()
 bma_accuracy_ts = np.mean(np.argmax(bma_predictions_ts, axis=1) == bma_targets) * 100
 bma_nll_ts = -np.mean(np.log(bma_predictions_ts[np.arange(bma_predictions_ts.shape[0]), bma_targets] + args.eps))
 bma_unc_ts = utils.calibration_curve(bma_predictions_ts, bma_targets, args.num_bins)
 bma_ece_ts = bma_unc_ts['ece']
-tmp_ = bma_temperature.item()
-
 
     
 if not args.ignore_wandb:
@@ -435,7 +445,7 @@ if not args.ignore_wandb:
     wandb.run.summary['bma accuracy w/ ts'] = bma_accuracy_ts
     wandb.run.summary['bma nll w/ ts'] = bma_nll_ts
     wandb.run.summary['bma ece w/ ts'] = bma_ece_ts
-    wandb.run.summary['bma temperature'] = tmp_
+    wandb.run.summary['bma temperature'] = bma_temperature.item()
 
 print("[BMA w/o TS Results]\n")
 tab_name = ["# of Models", "BMA Accuracy", "BMA NLL", "BMA ECE"]
@@ -446,7 +456,7 @@ print("-"*30)
 
 print("[BMA w/ TS Results]\n")
 tab_name = ["# of Models", "BMA Accuracy", "BMA NLL", "BMA ECE", "BMA Temperature"]
-tab_contents = [args.bma_num_models, format(bma_accuracy_ts, '.2f'), format(bma_nll_ts, '.4f'), format(bma_ece_ts, '.4f'), format(tmp_, '.4f')]
+tab_contents = [args.bma_num_models, format(bma_accuracy_ts, '.2f'), format(bma_nll_ts, '.4f'), format(bma_ece_ts, '.4f'), format(bma_temperature.item(), '.4f')]
 table = [tab_name, tab_contents]
 print(tabulate.tabulate(table, tablefmt="simple"))
 print("-"*30)

utils/sabma/sabma_utils.py
@@ -323,13 +323,16 @@ def eval_sabma(loader, sabma_model, params, criterion, device, num_bins=15, eps=
 def bma_sabma(te_loader, sabma_model, bma_num_models,
             num_classes, criterion, device,
             bma_save_path=None, eps=1e-8, num_bins=15,
-            validation=False, tr_layer="nl_ll"
+            validation=False, tr_layer="nl_ll",
+            ood_loader=None
             ):
     '''
     run bayesian model averaging in test step
     '''
     sabma_logits = np.zeros((len(te_loader.dataset), num_classes))
     sabma_predictions = np.zeros((len(te_loader.dataset), num_classes))
+    if ood_loader is not None:
+        ood_sabma_predictions =np.zeros((len(ood_loader.dataset), num_classes))
     with torch.no_grad():
         for i in range(bma_num_models):
             # if i == 0:
@@ -345,12 +348,17 @@ def bma_sabma(te_loader, sabma_model, bma_num_models,
                 torch.save(params, f'{bma_save_path}/bma_model-{i}.pt')
             
             res = eval_sabma(te_loader, sabma_model, params, criterion, device, num_bins, eps)
+            if ood_loader is not None:
+                ood_res = eval_sabma(ood_loader, sabma_model, params, criterion, device, num_bins, eps)
 
             if not validation:
                 print(f"SA-BMA Sample {i+1}/{bma_num_models}. Accuracy: {res['accuracy']:.2f}%  NLL: {res['nll']:.4f}")
 
             sabma_logits += res["logits"]
             sabma_predictions += res["predictions"]
+            
+            if ood_loader is not None:
+                ood_sabma_predictions += ood_res["predictions"]
 
             ens_accuracy = np.mean(np.argmax(sabma_predictions, axis=1) == res["targets"]) * 100
             ens_nll = -np.mean(np.log(sabma_predictions[np.arange(sabma_predictions.shape[0]), res["targets"]] / (i + 1) + eps))
@@ -360,25 +368,49 @@ def bma_sabma(te_loader, sabma_model, bma_num_models,
 
         sabma_logits /= bma_num_models
         sabma_predictions /= bma_num_models
+        
+        if ood_loader is not None:
+            ood_sabma_predictions /= bma_num_models
 
         sabma_loss = criterion(torch.tensor(sabma_predictions), torch.tensor(res['targets'])).item()
         sabma_accuracy = np.mean(np.argmax(sabma_predictions, axis=1) == res["targets"]) * 100
-        sabma_nll = -np.mean(np.log(sabma_predictions[np.arange(sabma_predictions.shape[0]), res["targets"]] + eps))
-        
+        sabma_nll = -np.mean(np.log(sabma_predictions[np.arange(sabma_predictions.shape[0]), res["targets"]] + eps))        
         unc = utils.calibration_curve(sabma_predictions, res["targets"], num_bins)
         sabma_ece = unc['ece']
         
+        if ood_loader is not None:
+            ood_sabma_accuracy = np.mean(np.argmax(ood_sabma_predictions, axis=1) == ood_res["targets"]) * 100
+            ood_sabma_nll = -np.mean(np.log(ood_sabma_predictions[np.arange(ood_sabma_predictions.shape[0]), ood_res["targets"]] + eps))
+            ood_sabma_unc = utils.calibration_curve(ood_sabma_predictions, ood_res["targets"], num_bins)
+            ood_sabma_ece = ood_sabma_unc['ece']
+        
     if not validation:
         print(f"bma Accuracy using {bma_num_models} model : {sabma_accuracy:.2f}% / NLL : {sabma_nll:.4f}")
-    
-    return {"logits" : sabma_logits,
+        
+    if ood_loader is not None:    
+        return {"logits" : sabma_logits,
             "predictions" : sabma_predictions,
             "targets" : res["targets"],
             "loss" : sabma_loss,
             "accuracy" : sabma_accuracy,
             "nll" : sabma_nll,
-            "ece" : sabma_ece
+            "ece" : sabma_ece,
+            "ood_predictions" : ood_sabma_predictions,
+            "ood_targets" : ood_res["targets"],
+            "ood_accuracy" : ood_sabma_accuracy,
+            "ood_nll" : ood_sabma_nll,
+            "ood_unc" : ood_sabma_unc,
+            "ood_ece" : ood_sabma_ece
             }
+    else:
+        return {"logits" : sabma_logits,
+            "predictions" : sabma_predictions,
+            "targets" : res["targets"],
+            "loss" : sabma_loss,
+            "accuracy" : sabma_accuracy,
+            "nll" : sabma_nll,
+            "ece" : sabma_ece,
+            }   
     
     
     
@@ -416,6 +448,8 @@ def save_best_sabma_model(args, best_epoch, sabma_model, optimizer, scaler, firs
                                 optimizer = optimizer.state_dict(),
                                 # scheduler = scheduler.state_dict(),
                                 )
+    torch.save(sabma_model, f"{args.save_path}/{args.method}-{args.optim}_best_val_model.pt")
+    
     # Save Mean, variance, Covariance matrix
     mean = sabma_model.get_mean_vector()
     torch.save(mean,f'{args.save_path}/{args.method}-{args.optim}_best_val_mean.pt')

utils/utils.py
@@ -658,14 +658,15 @@ def no_ts_map_estimation(args, te_loader, num_classes, model, mean, variance, cr
     return res
 
 
-def ts_map_estimation(args, val_loader, te_loader, num_classes, model, mean, variance, criterion):
+def ts_map_estimation(args, val_loader, te_loader, num_classes, model, mean, variance, criterion, save=True):
     # Temperature Scaled Results
     scaled_model = None
     if args.method in ["dnn", "swag", "ll_swag"]:
         scaled_model = ts.ModelWithTemperature(model)
         scaled_model.set_temperature(val_loader)
         temperature = scaled_model.temperature
-        torch.save(scaled_model, f"{args.save_path}/{args.method}-{args.optim}_best_val_scaled_model.pt")
+        if save:
+            torch.save(scaled_model, f"{args.save_path}/{args.method}-{args.optim}_best_val_scaled_model.pt")
         if not args.ignore_wandb:
             wandb.run.summary['temperature'] = temperature.item()
         res = eval(te_loader, scaled_model, criterion, args.device)   
@@ -675,8 +676,9 @@ def ts_map_estimation(args, val_loader, te_loader, num_classes, model, mean, var
         temperature = res["temperature"]
     else:
         raise NotImplementedError("Need Code for temperature scaling on this method")
-    
-    save_reliability_diagram(args.method, args.optim, args.save_path, res['unc'], False)
+
+    if save:
+        save_reliability_diagram(args.method, args.optim, args.save_path, res['unc'], False)
     
     return res, temperature
 
@@ -689,7 +691,7 @@ def bma(args, tr_loader, val_loader, te_loader, ood_loader, num_classes, model, 
     if args.method in ["swag", "ll_swag"]:
         bma_res = swag_utils.bma_swag(tr_loader, te_loader, ood_loader, model, num_classes, criterion, args.bma_num_models, bma_save_path, args.eps, args.batch_norm, num_bins=args.num_bins)       
     elif args.method in ["vi", "ll_vi"]:
-        bma_res = vi_utils.bma_vi(val_loader, te_loader, ood_loader, mean, variance, model, args.method, criterion, num_classes, None, args.bma_num_models, None, args.num_bins, args.eps)
+        bma_res = vi_utils.bma_vi(val_loader, te_loader, ood_loader, mean, variance, model, args.method, criterion, num_classes, None, args.bma_num_models, bma_save_path, args.num_bins, args.eps)
     else:
         raise NotImplementedError("Add code for Bayesian Model Averaging with Temperature scaling for this method")
     bma_logits = bma_res["logits"]; bma_targets = bma_res["targets"]
@@ -733,13 +735,16 @@ def bma(args, tr_loader, val_loader, te_loader, ood_loader, num_classes, model, 
         wandb.run.summary['bma ece w/ ts'] = bma_ece_ts
         wandb.run.summary['bma temperature'] = temperature.item()
         
-        wandb.rum.summary['ood bma accuracy'] = bma_res['ood_accuracy']
-        wandb.rum.summary['ood bma nll'] = bma_res['ood_nll']
-        wandb.rum.summary['ood bma ece'] = bma_res['ood_ece']
-        
-    save_reliability_diagram(args.method, args.optim, args.save_path, bma_res['unc'], True)
+        wandb.run.summary['ood bma accuracy'] = bma_res['ood_accuracy']
+        wandb.run.summary['ood bma nll'] = bma_res['ood_nll']
+        wandb.run.summary['ood bma ece'] = bma_res['ood_ece']
     
+    if not bma_save_path is not None:    
+        save_reliability_diagram(args.method, args.optim, args.save_path, bma_res['unc'], True)
     
+    return bma_res, bma_accuracy, bma_nll, bma_ece, bma_accuracy_ts, bma_nll_ts, bma_ece_ts, temperature.item(), bma_res['ood_accuracy'], bma_res['ood_nll'], bma_res['ood_ece']
+
+
 
 def topk_accuracy(output, target, topk=(1,5)):
     """Compute the top-k accuracy for the specified values of k"""