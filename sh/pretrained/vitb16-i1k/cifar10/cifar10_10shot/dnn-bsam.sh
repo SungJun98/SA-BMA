3c283da4ea23fd5672abbaa3470343c526f8900c
sooyonee <mulan98@yonsei.ac.kr>
2024-03-12 06:45:19 +0000

    bash file for vit-b16 i1k best config

---
+++ sh/pretrained/vitb16-i1k/cifar10/cifar10_10shot/dnn-bsam.sh
@@ -0,0 +1,11 @@
+# ------------------------------------------------------
+## conda activate /data1/lsj9862/anaconda3/envs/bsam (server 40)
+# ------------------------------------------------------
+for seed in 0 1 2
+do
+CUDA_VISIBLE_DEVICES=2 python3 run_baseline.py --method=dnn --optim=bsam --rho=0.01 --dataset=cifar10 --use_validation --dat_per_cls=10 \
+--model=vitb16-i1k --pre_trained  --lr_init=1e-1 --epochs=150 --wd=1e-2 --noise_scale=1e-2 \
+--scheduler=cos_decay --seed=${seed} --no_amp --warmup_t=10 --group=ViT-i1K-CIFAR10-best
+done
+# ------------------------------------------------------
+# ------------------------------------------------------
--- sh/pretrained/vitb16-i1k/cifar100/cifar100_10shot/dnn-bsam-1e-1.sh
+++ sh/pretrained/vitb16-i1k/cifar100/cifar100_10shot/dnn-bsam-1e-1.sh
@@ -7,13 +7,13 @@
 # ------------------------------------------------------
 for lr_init in 1e-1
 do
-for wd in 1e-2 1e-3 1e-4
+for wd in 1e-4 1e-3 1e-2
 do
-for rho in 0.01 0.05 0.1
+for rho in 0.1 0.05 0.01
 do
-for warmup_t in 10 20
+for warmup_t in 10
 do
-for noise_scale in 1e-2 1e-3 1e-4
+for noise_scale in 1e-4 1e-3 1e-2
 do
 CUDA_VISIBLE_DEVICES=0 python3 run_baseline.py --method=dnn --optim=bsam --rho=${rho} --dataset=cifar100 --use_validation --dat_per_cls=10 \
 --model=vitb16-i1k --pre_trained  --lr_init=${lr_init} --epochs=150 --wd=${wd} --noise_scale=${noise_scale} \
