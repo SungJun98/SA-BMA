b6a14ec74c25de56c7c2fead93b92da9cf93e11a
sooyonee <mulan98@yonsei.ac.kr>
2024-03-12 14:15:39 +0000

    sweep range for vitb16-i1k

--- sh/pretrained/vitb16-i1k/cifar100/cifar100_10shot/dnn-bsam-1e-1.sh
+++ sh/pretrained/vitb16-i1k/cifar100/cifar100_10shot/dnn-bsam-1e-1.sh
@@ -1,23 +1,24 @@
 # ------------------------------------------------------
 ## conda activate /data1/lsj9862/anaconda3/envs/bsam
+## (40) conda activate /data1/lsj9862/miniconda3/envs/bsam
 # ------------------------------------------------------
 
 # ------------------------------------------------------
 ## Coarse
 # ------------------------------------------------------
-for lr_init in 1e-1
+for lr_init in 0.2 0.25
 do
-for wd in 1e-4 1e-3 1e-2
+for wd in 1e-4 1e-3
 do
-for rho in 0.1 0.05 0.01
+for rho in 0.001 0.0005 0.0001 0.00005
 do
-for warmup_t in 10
+for damping in 0.001 0.005
 do
 for noise_scale in 1e-4 1e-3 1e-2
 do
-CUDA_VISIBLE_DEVICES=0 python3 run_baseline.py --method=dnn --optim=bsam --rho=${rho} --dataset=cifar100 --use_validation --dat_per_cls=10 \
+CUDA_VISIBLE_DEVICES=3 python3 run_baseline.py --method=dnn --optim=bsam --rho=${rho} --dataset=cifar100 --use_validation --dat_per_cls=10 \
 --model=vitb16-i1k --pre_trained  --lr_init=${lr_init} --epochs=150 --wd=${wd} --noise_scale=${noise_scale} \
---scheduler=cos_decay --no_amp --warmup_t=${warmup_t}
+--scheduler=cos_decay --no_amp --warmup_t=10 --no_bma --num_bma_models=1 --damping=${damping}
 done
 done
 done
