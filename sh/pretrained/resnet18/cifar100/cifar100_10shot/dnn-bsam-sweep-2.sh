e77236abf9f35bf4dbff40ec52fcede3357cf44e
sooyonee <mulan98@yonsei.ac.kr>
2024-03-17 15:27:28 +0000

    bash file for resnet18 cifar100 hyperparameter search

--- sh/pretrained/resnet18/cifar100/cifar100_10shot/dnn-bsam-sweep-2.sh
+++ sh/pretrained/resnet18/cifar100/cifar100_10shot/dnn-bsam-sweep-2.sh
@@ -6,7 +6,7 @@
 # ------------------------------------------------------
 ## Coarse
 # ------------------------------------------------------
-for lr_init in 0.5
+for lr_init in 1
 do
 for wd in 1e-2 1e-3 1e-4
 do
@@ -16,7 +16,7 @@ for damping in 0.1
 do
 for noise_scale in 1e-1 1e-2 1e-3
 do
-CUDA_VISIBLE_DEVICES=3 python3 run_baseline.py --method=dnn --optim=bsam --rho=${rho} --dataset=cifar100 --use_validation --dat_per_cls=10 \
+CUDA_VISIBLE_DEVICES=2 python3 run_baseline.py --method=dnn --optim=bsam --rho=${rho} --dataset=cifar100 --use_validation --dat_per_cls=10 \
 --model=resnet18 --pre_trained  --lr_init=${lr_init} --epochs=400 --wd=${wd} --noise_scale=${noise_scale} \
 --scheduler=cos_decay --no_amp --warmup_t=10 --bma_num_models=1 --damping=${damping} --no_ts --no_bma
 done
