9754158957a0ce9ce5d0dd50f5696ee800a1a4c5
emforce77 <86914655+emforce77@users.noreply.github.com>
2024-06-02 23:10:10 +0900

    Revert "memo"

--- BayesTFL2/priorBox/__init__.py
+++
@@ -1,8 +0,0 @@
-from .prior import Prior
-from .bayesian_learning import bayesian_learner
-
-from .options import options
-
-
-
-__all__ = ['Prior', 'bayesian_learner', 'options']
--- BayesTFL2/priorBox/prior/prior_ssl_utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .utils import load_weights2
-
-
-__all__ = ['load_weights2']
